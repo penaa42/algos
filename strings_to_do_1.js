86e642897b1792fdfd116019fc3d01f754eb53cb
Adrian Pena <adrianpena383@gmail.com>
2023-08-16 19:08:30 -0400

    string algo completed

--- strings_to_do_1.js
+++ strings_to_do_1.js
@@ -1,7 +1,6 @@
 // remove blanks
 function remove_blanks(str){
     let arr = str.split(" ")
-    // console.log(arr)
     console.log(arr.join(""))
 }
 // remove_blanks("Pl ayTha tF u nkyM usi c")
@@ -24,8 +23,71 @@ function get_digits(str){
     console.log(number.join(""))
 }
 
-get_digits("abc8c0d1ngd0j0!8")
+// get_digits("abc8c0d1ngd0j0!8")
 // expected output: 801008
-get_digits("Os1a3y5w7h9a2t4?6!8?0")
+// get_digits("Os1a3y5w7h9a2t4?6!8?0")
 // expected output: 1357924680
+
+
+// Acronyms
+function acronym(string){
+    let arr = string.split(" ")
+    // console.log(arr)
+
+    let output = []
+
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i].length; j++){
+            if (j == 0) {
+                output[i] = arr[i][j]
+            }
+        }
+    }
+    final = output.join("")
+    console.log(final.toUpperCase())
+}
+
+// acronym("there's no free lunk - gotta pay yer way.")
+// expected output: "TNFL-GPYW"
+// acronym("Live from New York, it's Saturday Night!")
+// expected output: "LFNYISN"
+
+
+// count non-spaces
+function countNonSpaces(string){
+    let count = 0
+
+    for (let i = 0; i < string.length; i++){
+        if (string[i] != " "){
+            count++
+        }
+    }
+    console.log(count)
+}
+
+// countNonSpaces("Honey pie, you are driving me crazy")
+// expected output: 29
+// countNonSpaces("Hello world!")
+// expected output: 11
+
+
+// remove shorter strings
+function removeShorterStrings(arr, num){
+    let output = []
+
+    let count = 0
+
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].length >= num){
+            output[count] = arr[i]
+            count++
+        }
+    }
+    console.log(output)
+}
+
+removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4)
+// expected output: ['Good morning', 'sunshine', 'Earth', 'says', 'hello']
+removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3)
+// expected output: ['There', 'bug', 'the', 'system']
 
