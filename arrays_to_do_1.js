c4068cdff20f70ff3e1f8f29bd08915436390903
Adrian Pena <adrianpena383@gmail.com>
2023-08-16 17:15:36 -0400

    algos completed

--- arrays_to_do_1.js
+++ arrays_to_do_1.js
@@ -11,8 +11,9 @@ function push_front(arr, num) {
     console.log('Final array output', arr);
 }
 // push_front([5,7,2,3], 8);
+//  expected output: [8,5,7,2,3]
 // push_front([99], 7);
-
+// expected output: [7,99]
 
 // Pop Front
 function pop_front(arr){
@@ -30,42 +31,36 @@ function pop_front(arr){
     console.log(final_arr)
     return arr_first
 }
-// pop_front([0,5,10,15])
 // console.log(pop_front([0,5,10,15]))
+// expected output: [5,10,15] return 0
 // console.log(pop_front([4,5,7,9]))
+// expected output [5,7,9] return 4
 
-
+// Insert at
 function insert_at(arr,val,num){
     let final_arr = []
 
     for (let i = 0; i < arr.length; i++){
-        console.log(`index: ${i}, value: ${arr[i]}`)
+        // console.log(`index: ${i}, value: ${arr[i]}`)
         let current = arr[i]
         let next = arr[i + 1]
+        let last = arr[arr.length-1]
 
-        console.log(current)
-        console.log(next)
-
-// != first then ==
-
+        if (i != val){
+            final_arr[i] = arr[i]
+        }
 
         if (i == val){
-            console.log(`condition met`)
             final_arr[i] = num
-            final_arr[i + 1] = next
-            // i++
-            // continue
+            final_arr[i + 1] = current
+            i++
         }
-        // else {
-            final_arr[i] = current
-        // }
-
+        final_arr[arr.length] = last
     }
     console.log('Final array', final_arr)
 }
-
 // insert_at([100,200,5],2,311)
-// expected output: [100,200,311,5]
+// // expected output: [100,200,311,5]
 
-insert_at([9,33,7],1,42)
+// insert_at([9,33,7],1,42)
 // expected output: [9,42,33,7]
